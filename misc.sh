fd28206e016d999837b347d878c84ac9ed5d2cb8
Eyal Perry <e.perry.pa@gmail.com>
2020-12-27 09:23:36 +0200

    docs

--- misc.sh
+++ misc.sh
@@ -2,6 +2,8 @@ alias cls="clear && printf '\e[3J'"
 alias dotfiles="cd $SCRIPT_ROOT"
 alias cr="cd $CODE_ROOT"
 
+# kills the process which is bound to the specified port
+# arg0: port of process to kill
 function pkill(){
     kill -9 $(lsof -t -i:$1)
 }
