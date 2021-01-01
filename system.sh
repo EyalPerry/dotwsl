d7c154174fca1fe3dd17be9ac89c8a1197dba43f
Eyal Perry <e.perry.pa@gmail.com>
2021-01-01 09:42:44 +0200

    add anaconda3 to path

--- system.sh
+++ system.sh
@@ -3,5 +3,9 @@
 ## load nvm
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
 
+if [ -d "$HOME/anaconda3" ] ; then
+  PATH="$PATH:$HOME/anaconda3"
+fi
+
 ## load ssh credentials
 eval "$(ssh-agent -s > /dev/null 2>&1)" > /dev/null 2>&1
