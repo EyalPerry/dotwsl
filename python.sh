15994d12d270993f32c7837530066d571c51d806
Eyal Perry <e.perry.pa@gmail.com>
2021-03-31 11:44:55 +0300

    use pytest

--- python.sh
+++ python.sh
@@ -1,4 +1,4 @@
 alias py_env_i="rm -rf .pyenv/env && python3 -m venv .pyenv/env"
 alias py_env="source ./.pyenv/env/bin/activate"
 alias py_i="py_env && pip3 install -e '.[dev]'"
-alias py_test="py_env && python3 -m unittest"
+alias py_test="py_env && pytest"
