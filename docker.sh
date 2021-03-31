4fef2f8796d731a8c744f9ac36fcc7e34eefb058
Eyal Perry <e.perry.pa@gmail.com>
2021-03-31 11:01:30 +0300

    more useful scripts

--- docker.sh
+++ docker.sh
@@ -1,5 +1,5 @@
-alias dstart="docker-compose up -t 0 --remove-orphans --force-recreate"
-alias dstop="docker-compose down -t 0 --remove-orphans"
+alias up="docker-compose up -t 0 --remove-orphans --force-recreate -d"
+alias down="docker-compose down -t 0 --remove-orphans"
 alias dkill="docker rm -f \$(docker ps -a -q)"
 alias dlog="docker-compose logs -f"
 alias dlogd="docker-compose logs"
--- python.sh
+++ python.sh
@@ -1,1 +1,4 @@
-# alias pyi="pip install -r requirements.txt"
+alias py_env_i="rm -rf .pyenv/env && python3 -m venv .pyenv/env"
+alias py_env="source ./.pyenv/env/bin/activate"
+alias py_i="py_env && pip3 install -e '.[dev]'"
+alias py_test="py_env && python3 -m unittest"
