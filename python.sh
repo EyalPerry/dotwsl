59cba233b00bdab9a3a903d91ced28e7711cb7b9
Eyal Perry <e.perry.pa@gmail.com>
2021-04-01 15:16:25 +0300

    pyi script and enable env by default if exists

--- python.sh
+++ python.sh
@@ -1,4 +1,15 @@
 alias py_env_i="rm -rf .pyenv/env && python3 -m venv .pyenv/env"
 alias py_env="source ./.pyenv/env/bin/activate"
-alias py_i="py_env && pip3 install -e '.[dev]'"
+
 alias py_test="py_env && pytest"
+
+function pyi(){
+   pip3 install -I -r requirements.txt
+   for d in src/functions/*/ ; do
+   pip3 install -I -r "$d"/requirements.txt
+   done
+}
+
+if [ -f ./.pyenv/env/bin/activate ]; then
+    py_env
+fi
