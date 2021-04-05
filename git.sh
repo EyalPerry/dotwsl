2db9d20e948f0d086eba120248ea10d978d5d9c3
Eyal Perry <e.perry.pa@gmail.com>
2021-04-05 13:14:34 +0300

    add ptag command for pushing tags

--- git.sh
+++ git.sh
@@ -13,13 +13,13 @@ if main_exists; then
     export DEFAULT_BRANCH="main"
 fi
 
-function current_branch(){
+function current_branch() {
     git symbolic-ref -q HEAD | sed -e 's|^refs/heads/||'
 }
 
 ### Stage all and commit with message
 ### arg0-n: commit message parts
-function commit() { 
+function commit() {
     git add -A && git commit -m "$*"
 }
 
@@ -27,33 +27,44 @@ function commit() {
 ### arg0: remote url
 ### arg1: remote name (defaults to origin)
 function set_remote() {
-    REMOTE=${2:-origin} 
+    REMOTE=${2:-origin}
     git remote set-url $REMOTE $1
 }
 
 ### creates a local branch from a source branch after pulling it.
 ### arg0: name of branch to create
 ### arg1: name of source branch (defaults to main)
-function branch(){
-   SRC=${2:-$DEFAULT_BRANCH} 
-   git checkout $SRC && git pull \
-   && git checkout -b $1 
+function branch() {
+    SRC=${2:-$DEFAULT_BRANCH}
+    git checkout $SRC && git pull &&
+        git checkout -b $1
 }
 
 ### publishes the current branch to a remote repository
 ### arg0: remote repository (defaults to origin)
-function publish(){
-   REMOTE=${1:-origin}
-   X_CURRENT_GIT_BRANCH=$(current_branch)
-   git push -u $REMOTE $X_CURRENT_GIT_BRANCH
+function publish() {
+    REMOTE=${1:-origin}
+    X_CURRENT_GIT_BRANCH=$(current_branch)
+    git push -u $REMOTE $X_CURRENT_GIT_BRANCH
 }
 
 ### pulls and merges a source branch into the current branch
 ### arg0: name of the source branch (defaults to main)
-function sync(){
-    SRC=${1:-$DEFAULT_BRANCH} 
+function sync() {
+    SRC=${1:-$DEFAULT_BRANCH}
     X_CURRENT_GIT_BRANCH=$(current_branch)
-    git checkout $SRC && git pull \
-    && git checkout $X_CURRENT_GIT_BRANCH \
-    && git merge --no-edit $SRC
+    git checkout $SRC && git pull &&
+        git checkout $X_CURRENT_GIT_BRANCH &&
+        git merge --no-edit $SRC
+}
+
+function ptag() {
+    if [ -z $1 ]; then
+        echo "no argument supplied"
+        exit 1
+    fi
+
+    git push --delete origin $1
+    git tag -f $1
+    git push origin $1
 }
