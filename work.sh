1ae1ae0bc3e7d61bdaa3fb53560761de0f331a96
Eyal Perry <e.perry.pa@gmail.com>
2021-04-25 10:10:19 +0300

    add website run command

--- work.sh
+++ work.sh
@@ -1,5 +1,3 @@
-
-
 function ag_cloud() {
     cd $CODE_ROOT && cd agwa-cloud-components
 }
@@ -8,7 +6,8 @@ function ag_gg() {
     cd $CODE_ROOT && cd agwa-greengrass-service
 }
 
-alias agrw="cr && cd agwa-agronomist-web && start"
+alias agrw="cr && cd agwa-agronomist-web && nkill && start"
+alias agpw="cr && cd agwa-website/client && nkill && start"
 
 export AGWA_SERVICE_LIBRARY_TAG=edge
 alias aglib="python3 -m pip install git+ssh://git@github.com/agwafarm/agwa-service-library.git@${AGWA_SERVICE_LIBRARY_TAG}#egg=agwa-service-library"
